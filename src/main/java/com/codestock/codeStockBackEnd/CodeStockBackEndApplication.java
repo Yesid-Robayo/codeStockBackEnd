aa66ad2daa80a057774feba79aa59121f453bb5b
Yesid11 <yesidrobayo11@gmail.com>
2024-05-10 13:10:07 -0500

    Inicio Repositorio

---
+++ src/main/java/com/codestock/codeStockBackEnd/CodeStockBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.codestock.codeStockBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeStockBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeStockBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codestock/codeStockBackEnd/CodeStockBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestock.codeStockBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeStockBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
